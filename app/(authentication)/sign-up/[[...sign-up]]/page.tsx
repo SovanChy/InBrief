7e7af44bca62ee3826a1341be10a60cc6c9b974f
Luvion007 <chysovan007@gmail.com>
2025-03-06 15:36:25 +0700

    test

---
+++ app/(authentication)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+ 'use client'
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+    
+
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <SignUp forceRedirectUrl={"/"}/> {/* Redirect after successful sign-up */}
+        </div>
+    )
+}
