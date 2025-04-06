0e9abc90e9156fa803f5453c4063d2c98d43b4bd
Luvion007 <chysovan007@gmail.com>
2025-04-06 18:54:03 +0700

    finalization of the project

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,7 @@
 // app/dashboard/loading.js
 export default function Loading() {
     return (
+        // loading screen
         <div className="flex flex-col justify-center items-center h-screen">
         <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin mb-4"></div>
         <div className="text-gray-700 mb-4">Loading</div>     \ </div>
